56b0819d50c5c93ec118b20d11652c8ebb1dd887
Aarkan <johanwiren1@gmail.com>
2019-04-01 13:59:21 +0200

    step test

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,10 +2,8 @@
   <div class="about">
     <h1>This is an about page</h1>
     <br>
-    <h3>Gyroscope: {{Gyroscope}}</h3>
-    <h3>Left/Right: {{leftRight}}</h3>
-    <h3>Front/Back: {{frontBack}}</h3>
-    <h3>Dir: {{dir}}</h3>
+    <h3>Gyroscope: {{gyro}}</h3>
+    <h3>Steps: {{steps}}</h3>
   </div>
 </template>
 
@@ -13,10 +11,8 @@
 export default {
   data() {
     return {
-      Gyroscope: 0,
-      leftRight: 0,
-      frontBack: 0,
-      dir: 0
+      gyro: 0,
+      steps: 0
     };
   },
   created() {
@@ -28,14 +24,12 @@ export default {
       let y = gyroscope.y;
       let z = gyroscope.z;
 
-      this.Gyroscope = Math.sqrt(x * x + y * y + z * z);
+      this.gyro = Math.sqrt(x * x + y * y + z * z);
 
-      this.leftRight = gyroscope.x.toFixed(4);
-      this.frontBack = gyroscope.y.toFixed(4);
-      this.dir = gyroscope.z.toFixed(4);
+      if (this.gyro > 1) this.steps++;
     };
     gyroscope.onerror = e => {
-      this.leftRight = "ERROR";
+      this.gyro = "ERROR";
     };
   }
 };
