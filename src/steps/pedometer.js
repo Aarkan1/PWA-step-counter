67c2580ffde38e36cb910129813608b3e7bcc927
Aarkan <johanwiren1@gmail.com>
2019-04-01 12:54:43 +0200

    initial commit

---
+++ src/steps/pedometer.js
@@ -0,0 +1,8 @@
+export default class Perdometer {
+
+    constructor(){
+        
+    }
+
+
+}
---
+++ src/views/About.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <br>
+    <h3>Left/Right: {{leftRight}}</h3>
+    <h3>Front/Back: {{frontBack}}</h3>
+    <h3>Dir: {{dir}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      leftRight: 0,
+      frontBack: 0,
+      dir: 0
+    };
+  },
+  created() {
+    let gyroscope = new Gyroscope();
+    gyroscope.start();
+
+    gyroscope.onreading = e => {
+      this.leftRight = gyroscope.x.toFixed(4);
+      this.frontBack = gyroscope.y.toFixed(4);
+      this.dir = gyroscope.z.toFixed(4);
+    };
+    gyroscope.onerror = e => {
+      this.leftRight = "ERROR";
+    };
+  }
+};
+</script>
